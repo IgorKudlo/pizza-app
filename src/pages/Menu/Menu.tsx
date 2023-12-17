ca6e687981c53793f4c46aaeaca6026db46d158d
igor.kudlo <igoreklida@gmail.com>
2023-12-17 23:06:09 +0300

    request

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -2,23 +2,49 @@ import styles from './Menu.module.scss';
 import { Heading } from '@/components/Heading';
 import { Search } from '@/components/Search';
 import { ProductCard } from '@/components/ProductCard';
+import { PREFIX } from '@/helpers/API';
+import { useEffect, useState } from 'react';
+import { Product } from '@/interfaces/product';
 
 
 export const Menu = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  const getMenu = async () => {
+    try {
+      const res = await fetch(`${PREFIX}/products`);
+      if (!res.ok) {
+        return;
+      }
+      const data = await res.json() as Product[];
+      setProducts(data);
+    } catch (e) {
+      console.error(e);
+      return;
+    }
+  };
+
+  useEffect(() => {
+    getMenu();
+  }, []);
+
   return (<>
     <div className={styles['head']}>
       <Heading>Меню</Heading>
       <Search placeholder='Введите блюдо или состав'/>
     </div>
     <div>
-      <ProductCard
-        id={1}
-        title='Наслаждение'
-        description='Салями, руккола, помидоры, оливки'
-        rating={4.5}
-        price={300}
-        image='/product-demo.png'
-      />
+      {products.map((p) => (
+        <ProductCard
+          key={p.id}
+          id={p.id}
+          name={p.name}
+          description={p.ingredients.join(', ')}
+          rating={p.rating}
+          price={p.price}
+          image={p.image}
+        />
+      ))}
     </div>
   </>);
 };
