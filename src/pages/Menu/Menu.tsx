fb25a5b09f3b2e3802b982b93f427876954f8e90
igor.kudlo <igoreklida@gmail.com>
2023-12-17 20:33:46 +0300

    ProductCard

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -1,13 +1,24 @@
 import styles from './Menu.module.scss';
 import { Heading } from '@/components/Heading';
 import { Search } from '@/components/Search';
+import { ProductCard } from '@/components/ProductCard';
 
 
 export const Menu = () => {
-  return (
+  return (<>
     <div className={styles['head']}>
       <Heading>Меню</Heading>
       <Search placeholder='Введите блюдо или состав'/>
     </div>
-  );
+    <div>
+      <ProductCard
+        id={1}
+        title='Наслаждение'
+        description='Салями, руккола, помидоры, оливки'
+        rating={4.5}
+        price={300}
+        image='/product-demo.png'
+      />
+    </div>
+  </>);
 };
