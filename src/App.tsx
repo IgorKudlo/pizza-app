c5a72af27db37707e47ac13bb972959e71dad314
igor.kudlo <igoreklida@gmail.com>
2023-12-17 19:10:05 +0300

    Outlet

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,10 @@
 import { Button } from '@/components/Button';
 import { Input } from '@/components/Input';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { Menu } from '@/pages/Menu';
-import { Cart } from '@/pages/Cart';
-import { Error } from '@/pages/Error';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Menu/>
-  },
-  {
-    path: '/cart',
-    element: <Cart/>
-  },
-  {
-    path: '*',
-    element: <Error/>
-  }
-]);
 
 function App() {
 
   return (
     <>
-      <div>
-        <a href="/">Меню</a>
-        <a href="/cart">Корзина</a>
-      </div>
-      <RouterProvider router={router} />
       <Button>Button</Button>
       <Button appearance={'big'}>Button</Button>
       <Input />
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,35 @@
 import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { Menu } from '@/pages/Menu';
+import { Cart } from '@/pages/Cart';
+import { Error } from '@/pages/Error';
+import { Layout } from '@/layout/Menu';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout/>,
+    children: [
+      {
+        path: '/',
+        element: <Menu/>
+      },
+      {
+        path: '/cart',
+        element: <Cart/>
+      }
+    ]
+  },
+  {
+    path: '*',
+    element: <Error/>
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
