ab5e9191de1431b18b3c1b792d34b87bfc22c787
igor.kudlo <igoreklida@gmail.com>
2023-12-17 17:55:02 +0300

    add router, old version with jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import { Button } from '@/components/Button';
 import { Input } from '@/components/Input';
+import { Link, Route, Routes } from 'react-router-dom';
+import { Menu } from '@/pages/Menu';
+import { Cart } from '@/pages/Cart';
+import { Error } from '@/pages/Error';
 
 function App() {
 
   return (
     <>
+      <div>
+        <Link to="/">Меню</Link>
+        <Link to="/cart">Корзина</Link>
+      </div>
+      <Routes>
+        <Route path={'/'} element={<Menu/>} />
+        <Route path={'/cart'} element={<Cart/>} />
+        <Route path={'*'} element={<Error/>} />
+      </Routes>
       <Button>Button</Button>
       <Button appearance={'big'}>Button</Button>
       <Input />
