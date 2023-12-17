a89baf2f64fa60342f1a059494439abd22753c55
igor.kudlo <igoreklida@gmail.com>
2023-12-17 19:53:03 +0300

    NavLink instead Link and useLocation

--- src/layout/Menu/Layout.tsx
+++ src/layout/Menu/Layout.tsx
@@ -1,11 +1,9 @@
 import styles from './Layout.module.scss';
-import { Link, Outlet, useLocation } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
 import cn from 'classnames';
 import { Button } from '@/components/Button';
 
 export const Layout = () => {
-  const location = useLocation();
-
   return (
     <div className={styles['layout']}>
       <div className={styles['sidebar']}>
@@ -15,13 +13,21 @@ export const Layout = () => {
           <div className={styles['email']}>alari@ya.ru</div>
         </div>
         <div className={styles['menu']}>
-          <Link to='/' className={cn(styles['link'], {
-            [styles.active]: location.pathname === '/'
-          })}>
+          <NavLink
+            to='/'
+            className={({ isActive }) => cn(styles['link'], {
+              [styles.active]: isActive
+            })}
+          >
             <img src="/menu-icon.svg" alt="Иконка меню" />
-            Меню</Link>
-          <Link to='/cart' className={styles['link']}>
-            <img src="/cart-icon.svg" alt="Иконка корзины" />Корзина</Link>
+            Меню</NavLink>
+          <NavLink
+            to='/cart'
+            className={({ isActive }) => cn(styles['link'], {
+              [styles.active]: isActive
+            })}
+          >
+            <img src="/cart-icon.svg" alt="Иконка корзины" />Корзина</NavLink>
         </div>
         <Button className={styles['exit']}>
           <img src="/exit-icon.svg" alt="Иконка выхода" />
