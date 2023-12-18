d19e10c6b47943d45a0fe7b350fd859cb85fed72
igor.kudlo <igoreklida@gmail.com>
2023-12-18 10:48:48 +0300

    login form

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,7 +1,34 @@
+import styles from './Login.module.scss';
+import { FormEvent } from 'react';
+import { Link } from 'react-router-dom';
+import { Button } from '@/components/Button';
+import { Heading } from '@/components/Heading';
+import { Input } from '@/components/Input';
+
 export const Login = () => {
+  const submit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log(e);
+  };
+
   return (
-    <div>
-      Login
+    <div className={styles['login']} onSubmit={submit}>
+      <Heading>Вход</Heading>
+      <form className={styles['form']}>
+        <div className={styles['field']}>
+          <label htmlFor="email">Ваш email</label>
+          <Input id="email" placeholder='Email'/>
+        </div>
+        <div className={styles['field']}>
+          <label htmlFor="password">Ваш пароль</label>
+          <Input id="password" type="password" placeholder='Пароль'/>
+        </div>
+        <Button appearance="big">Вход</Button>
+      </form>
+      <div className={styles['links']}>
+        <div>Нет акканута?</div>
+        <Link to="/auth/register">Зарегистрироваться</Link>
+      </div>
     </div>
   );
 };
