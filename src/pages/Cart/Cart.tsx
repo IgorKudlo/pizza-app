517afda121e27726305160149f2de0ce8ce5a21d
igor.kudlo <igoreklida@gmail.com>
2023-12-20 00:31:56 +0300

    Edit cart

--- src/pages/Cart/Cart.tsx
+++ src/pages/Cart/Cart.tsx
@@ -1,7 +1,39 @@
+import styles from './Cart.module.scss';
+import { useSelector } from 'react-redux';
+import { Heading } from '@/components/Heading/Heading';
+import { RootState } from '@/store/store';
+import { CartItem } from '@/components/CartItem';
+import { useEffect, useState } from 'react';
+import { ProductInterface } from '@/interfaces/product.interface';
+import axios from 'axios';
+import { PREFIX } from '@/helpers/API';
+
 export const Cart = () => {
-  return (
-    <div>
-      Cart
-    </div>
-  );
+  const [cartProducts, setCardProducts] = useState<ProductInterface[]>([]);
+  const items = useSelector((s: RootState) => s.cart.items);
+
+  const getItem = async (id: number) => {
+    const { data } = await axios.get<ProductInterface>(`${PREFIX}/products/${id}`);
+    return data;
+  };
+
+  const loadAllItems = async () => {
+    const res = await Promise.all(items.map(i => getItem(i.id)));
+    setCardProducts(res);
+  };
+
+  useEffect(() => {
+    loadAllItems();
+  }, [items]);
+
+  return <>
+    <Heading className={styles['heading']}>Корзина</Heading>
+    {items.map(i => {
+      const product = cartProducts.find(p => p.id === i.id);
+      if (!product) {
+        return;
+      }
+      return <CartItem key={product.id} count={i.count} {...product} />;
+    })}
+  </>;
 };
