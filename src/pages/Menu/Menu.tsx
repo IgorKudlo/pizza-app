f34b9db7a6917842dc37b773d76d282472a58671
igor.kudlo <igoreklida@gmail.com>
2023-12-19 23:34:36 +0300

    Search

--- src/pages/Menu/Menu.tsx
+++ src/pages/Menu/Menu.tsx
@@ -2,7 +2,7 @@ import styles from './Menu.module.scss';
 import { Heading } from '@/components/Heading';
 import { Search } from '@/components/Search';
 import { PREFIX } from '@/helpers/API';
-import { useEffect, useState } from 'react';
+import { ChangeEvent, useEffect, useState } from 'react';
 import { ProductInterface } from '@/interfaces/product.interface';
 import axios, { AxiosError } from 'axios';
 import { MenuList } from '@/pages/Menu/MenuList';
@@ -12,11 +12,16 @@ export const Menu = () => {
   const [products, setProducts] = useState<ProductInterface[]>([]);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [error, setError] = useState<string | undefined>();
+  const [filter, setFilter] = useState<string>();
 
-  const getMenu = async () => {
+  const getMenu = async (name?: string) => {
     try {
       setIsLoading(true);
-      const { data } = await axios.get<ProductInterface[]>(`${PREFIX}/products`);
+      const { data } = await axios.get<ProductInterface[]>(`${PREFIX}/products`, {
+        params: {
+          name
+        }
+      });
       setProducts(data);
     } catch (e) {
       if (e instanceof AxiosError) {
@@ -27,19 +32,24 @@ export const Menu = () => {
     }
   };
 
+  const updateFilter = (e: ChangeEvent<HTMLInputElement>) => {
+    setFilter(e.target.value);
+  };
+
   useEffect(() => {
-    getMenu();
-  }, []);
+    getMenu(filter);
+  }, [filter]);
 
   return (<>
     <div className={styles['head']}>
       <Heading>Меню</Heading>
-      <Search placeholder='Введите блюдо или состав'/>
+      <Search placeholder='Введите блюдо или состав' onChange={updateFilter} />
     </div>
     <div>
       {error && <>{error}</>}
-      {isLoading && <>Загружаем продукт...</>}
-      {!isLoading && <MenuList products={products}/>}
+      {!isLoading && products.length > 0 && <MenuList products={products} />}
+      {isLoading && <>Загружаем продукты...</>}
+      {!isLoading && products.length === 0 && <>Не найдено блюд по запросу</>}
     </div>
   </>);
 };
